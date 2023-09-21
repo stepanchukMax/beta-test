19212cc6dacc25075bb89e2ff8a8cdb9043072a8
bogdanidzze <bohdantrots@gmail.com>
2023-09-21 16:23:47 +0300

    add-slider

--- js/mobileSlider.js
+++ js/mobileSlider.js
@@ -2,13 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     var mySwiper = new Swiper(".swiper-container", {
         loop: true,
         autoplay: {
-            delay: 8000, // Таймер на 8 секунд
+            delay: 5000,
         },
         pagination: {
             el: ".swiper-pagination",
         },
     });
 
-    // Запускаємо таймер автопрокрутки слайдів
     mySwiper.autoplay.start();
 });
